f9e198aa7b2d91195688be264e2672a254ef7cbe
Kyle Kowalczyk <32282453+superadm1n@users.noreply.github.com>
2019-04-11 20:57:01 -0500

    Create logger_example.py

---
+++ logger_example.py
@@ -0,0 +1,24 @@
+import logging
+
+logfile = 'example.log'
+log_level = logging.INFO
+
+logger = logging.getLogger('App_name')
+logger.setLevel(log_level)
+
+file_handler = logging.FileHandler(logfile)
+file_handler.setLevel(log_level)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(log_level)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', '%d-%m-%Y %H:%M:%S')
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
+logger.info('example info message')
+logger.debug('example debug message')
+logger.warning('example warning message')
